4d9cc6c6b502cd47739942ebcaa3100a9d904c1e
JessTh <jessika.thunman@gmail.com>
2016-12-15 12:09:30 +0100

    tab -> space

--- google/geocode.js
+++ google/geocode.js
@@ -1,19 +1,19 @@
-var http				= require('http');
-var fs					= require('fs');
+var http        = require('http');
+var fs          = require('fs');
 
-var url1				= 'http://maps.googleapis.com/maps/api/geocode/json?address=';
-var url2				= '&sensor=false';
+var url1        = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
+var url2        = '&sensor=false';
 
-var file				= './' + process.argv[2];
-var outputFile	= process.argv[3] || './output.txt'
+var file        = './' + process.argv[2];
+var outputFile  = process.argv[3] || './output.txt'
 
 /*
 * Arguments: Input file, Output file (optional)
 * Read the file, process the adresses, make an API call every 2 seconds and write the output to the output file (default output.txt).
 * Input file format: ID, AdressPart1, AddressPart2, .... AddressPartN.
 * Output File format:
-* 	- Success	: ID, Longitude, Latitude, Location Type
-* 	- Error		: ID, , ,[ZERO_RESULTS]
+*   - Success  : ID, Longitude, Latitude, Location Type
+*   - Error    : ID, , ,[ZERO_RESULTS]
 */
 
 /* 2 seconds between calls due to API limtation */
@@ -47,37 +47,37 @@ function formatAddress(a) {
 
 function geocode(id, url) {
   http.get(url, function(res) {
-    var body = '';
-    var result = {};
+    var body    = '';
+    var result  = {};
     //console.log('STATUS: ' + res.statusCode);
     res.on('error', function(error) {
-    console.log(error);
-  });
+      console.log(error);
+    });
 
-  res.on('data', function(chunk) {
-    body += chunk;
-  });
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
 
-  res.on('end', function() {
-    result = JSON.parse(body);
-    var newLine = id + ',';
+    res.on('end', function() {
+      result = JSON.parse(body);
+      var newLine = id + ',';
 
-    switch(result.status) {
-      case 'ZERO_RESULTS':
-        newLine += ',,ZERO_RESULTS\n';
-        break;
+      switch(result.status) {
+        case 'ZERO_RESULTS':
+          newLine += ',,ZERO_RESULTS\n';
+          break;
 
-      case 'OK':
-        newLine += result.results[0].geometry.location.lat + ',';
-        newLine += result.results[0].geometry.location.lng + ',';
-        newLine += result.results[0].geometry.location_type + '\n';
-        break;
+        case 'OK':
+          newLine += result.results[0].geometry.location.lat + ',';
+          newLine += result.results[0].geometry.location.lng + ',';
+          newLine += result.results[0].geometry.location_type + '\n';
+          break;
 
-      default:
-        newLine += result.error_message + '\n';
-    }
-    fs.appendFile(outputFile, newLine);
-  });
+        default:
+          newLine += result.error_message + '\n';
+      }
+      fs.appendFile(outputFile, newLine);
+    });
 
   });
 };
